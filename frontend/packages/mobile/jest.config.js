bfd2e8d3a0f7334c49366d2c1279ca7189239530
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2026-05-28 15:44:51 -0300

    fix(mobile): move transformIgnorePatterns to jest.config.js (conflict with package.json jest key)

--- frontend/packages/mobile/jest.config.js
+++ frontend/packages/mobile/jest.config.js
@@ -2,6 +2,9 @@
 
 module.exports = {
   preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!(.pnpm/[^/]+/node_modules/)?(jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@shopify/.*)',
+  ],
   testMatch: ['**/__tests__/**/*.test.(ts|tsx|js)'],
   setupFilesAfterEnv: ['./jest.setup.js'],
   moduleNameMapper: {
